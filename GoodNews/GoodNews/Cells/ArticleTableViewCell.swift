34ac95f532d30162ff09cd759fb1cf0f2e75b1ac
eonsubae <eonsubae@naver.com>
2021-01-13 08:36:21 +0900

    Creating Custom Self Sizing Cells

---
+++ GoodNews/GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by KRITSSEAN on 2021/01/13.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+}
